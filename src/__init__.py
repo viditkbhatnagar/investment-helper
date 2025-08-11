6ebe4fcc00d3cfac4d5b58b4108c22458e42b198
Vidit Bhatnagar <bhatnagar007vidit@gmail.com>
2025-08-11 17:48:37 +0530

    trying now

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,10 @@
-"""
-Investment-Advisor source package.
+# """
+# Investment-Advisor source package.
 
-This makes the `src` folder importable, e.g.:
+# This makes the `src` folder importable, e.g.:
 
-    from src import models, live_data
-"""
+#     from src import models, live_data
+# """
 
-# Optionally, expose top-level modules for convenience
-from . import live_data, feature_engineering, recommendation_engine  # noqa: F401
+# # Optionally, expose top-level modules for convenience
+# from . import live_data, feature_engineering, recommendation_engine  # noqa: F401
